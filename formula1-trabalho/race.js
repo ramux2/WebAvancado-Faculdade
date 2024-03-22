3851fc65c5813400b7a736a2d652069b41842191
Guilherme <guiramos027@gmail.com>
2024-03-22 14:51:01 -0300

    projeto finalizado

--- formula1-trabalho/race.js
+++ formula1-trabalho/race.js
@@ -1,5 +1,7 @@
+var walletAmount = 100;
+let driverWinner = "";
+
 function init() {
-    var walletAmount = 100;
     const car = document.querySelectorAll(".car");
     car.forEach(function(car) {
         car.style.transform = "translate(" + 20 + "px, " + 20 + "px)"
@@ -9,28 +11,49 @@ function init() {
 
 function startRace() {
     const cars = document.querySelectorAll(".car");
+    driverWinner = "";
     cars.forEach(function(car) {
         moveCars(car);
     })
-    var betAmountInput = document.getElementById("amountInput");
-    var betAmount = betAmountInput.value
-
-    
-    bet(betAmount);
 }
 
 function moveCars(car) {
     const myInterval = setInterval(myTimer, 100);
     const raceTrackLimit = document.querySelector(".race-track").offsetWidth - car.offsetWidth
     let posX = 20;
+    var podium = [];
 
     function myTimer() {
         if (posX >= raceTrackLimit) {
             myStop(myInterval);
-            defineWinner(car.id);
+            podium.push(car.id)
+            if (!driverWinner) {
+                driverWinner = podium[0];
+                alert('O vencedor da corrida é ' + driverWinner);
+
+                var betAmountInput = document.getElementById("amountInput");
+                var betAmount = betAmountInput.value;
+
+                var select = document.getElementById("drivers-list");
+                var driverSelected = select.options[select.selectedIndex].value;
+
+                console.log(driverWinner);
+                console.log(driverSelected);
+                console.log(betAmount);
+                if (driverWinner == driverSelected) {
+                    var betProfit = betAmount * 2;
+                    walletAmount += betProfit;
+                    updateWalletAmount();
+                    console.log("Retorno da aposta: " + betProfit)
+                    return betProfit;
+                } else {
+                    console.log("Errou aposta")
+                }
+            }
         }
         posX += Math.ceil(Math.random() * 10);
         car.style.transform = "translate(" + posX + "px, " + 20 + "px)"
+
     }
 }
 
@@ -38,14 +61,22 @@ function myStop(raceInteval) {
     clearInterval(raceInteval);
 }
 
-function defineWinner(carId) {
-    let podium = [];
-    podium.push(carId);
-    podium.forEach(function(element) {
-        console.log(element);
-    });
+function bet() {
+    var betAmountInput = document.getElementById("amountInput");
+    var betAmount = betAmountInput.value;
+    
+    
+    if (isNaN(betAmount) || betAmount <= 0 || betAmount > walletAmount) {
+        alert('Aposta inválida!');
+        return;
+    }
+
+    walletAmount -= betAmount;
+    updateWalletAmount();
+    
+    startRace();
 }
 
-function bet(betAmount, driver) {
-    walletAmount = walletAmount - betAmount;
+function updateWalletAmount() {
+    document.getElementById("walletValue").innerHTML = "Valor da carteira: R$" + walletAmount;
 }
